c031c4b11aea4f97afe5d3d75e2193a58d7b35f8
kujira12 <ha9botan@gmail.com>
2024-03-26 17:31:43 +0900

    (ツイートアプリケーション課題再提出)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_25_035811) do
+ActiveRecord::Schema.define(version: 2024_03_26_082516) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
